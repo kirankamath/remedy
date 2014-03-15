56a3bad210dc5195e9de9eb4d9d4c7ae79ad3394
Kiran Kamath <kamath.kiran@gmail.com>
2014-03-15 13:52:06 -0700

    Implement filtering in remedy list

--- src/org/remedy/RemedyListActivity.java
+++ src/org/remedy/RemedyListActivity.java
@@ -2,11 +2,17 @@ package org.remedy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class RemedyListActivity extends Activity {
+
+    private ListView remedyList;
+    private EditText inputSearch;
+    private ArrayAdapter<String> remedyListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,28 @@ public class RemedyListActivity extends Activity {
 
         String[] remedyStrList = new String[] {"abies canadensis",
                 "abies nigra", "abrotanum", "absinthium"};
-        ListView remedyList = (ListView) findViewById(R.id.remedy_list);
-        ListAdapter listAdapter = new ArrayAdapter<String>(this,
+        remedyList = (ListView) findViewById(R.id.remedy_list);
+        inputSearch = (EditText) findViewById(R.id.remedy_list_inputSearch);
+
+        remedyListAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, remedyStrList);
-        remedyList.setAdapter(listAdapter);
+        remedyList.setAdapter(remedyListAdapter);
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                RemedyListActivity.this.remedyListAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
     }
 }
